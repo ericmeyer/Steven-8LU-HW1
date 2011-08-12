1304456d1d29c489eeffcb9c332f461441d3f606
Eric Meyer <emeyer@8thlight.com>
2011-08-12 07:40:31 -0500

    first test

---
+++ spec/hw1/core_spec.clj
@@ -0,0 +1,13 @@
+(ns core-spec
+  (:use [speclj.core]
+        [hw1.core])
+  (:require [hw1.core]))
+  
+  
+(describe "foo"
+  (it "is false"
+    (should true))
+
+)
+
+(run-specs)
--- test/hw1/test/core.clj
+++
@@ -1,6 +0,0 @@
-(ns hw1.test.core
-  (:use [hw1.core])
-  (:use [clojure.test]))
-
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
