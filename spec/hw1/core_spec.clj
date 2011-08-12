66bbcdb425753e25e6cd60afff05172ad242d25e
Eric Meyer <emeyer@8thlight.com>
2011-08-12 08:49:00 -0500

    hw done

--- spec/hw1/core_spec.clj
+++ spec/hw1/core_spec.clj
@@ -4,10 +4,23 @@
   (:require [hw1.core]))
   
   
-(describe "foo"
-  (it "is false"
+(describe "reverse-list"
+  (it "is true"
     (should true))
-
+  (it "reverses a single element list"
+    (should= '(1) (reverse-list '(1))))
+  (it "reverses a two element list"
+    (should= '(2 1) (reverse-list '(1 2))))
+  (it "reverses a list with a function call"
+    (should= (list (quote +) 1 2) (reverse-list (list 2 1 (quote +)))))
+    
+    
 )
+
+(describe "reverse-call"
+  (it "evals an addition"
+    (should= 3 (reverse-call (2 1 +))))
+)
+
 
 (run-specs)
--- src/hw1/core.clj
+++ src/hw1/core.clj
@@ -1,1 +1,7 @@
 (ns hw1.core)
+
+(defn reverse-list [list]
+  (reverse list))
+  
+(defmacro reverse-call [backwards-list]
+  (reverse-list backwards-list))
